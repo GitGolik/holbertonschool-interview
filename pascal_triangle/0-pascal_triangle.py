a102f0e6e9b400ce9399ca3484461880b20d5404
GitGolik <goliktheo.pro@gmail.com>
2026-09-11 11:11:02 +0200

    FIX: fix indentation

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """Module containing the creation of pascal triangle"""
 
+
 def pascal_triangle(n):
     """return pascal triangle in form of list of int"""
 
     if n <= 0:
-        return[]
+        return []
 
     triangle = [[1]]
 
     for i in range(1, n):
         prev = triangle[-1]
         row = [1]
-
 
         for j in range(1, i):
             row.append(prev[j - 1] + prev[j])
