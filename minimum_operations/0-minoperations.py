344309c6d6d980a51c026c2f1d024c65bdc5b211
GitGolik <goliktheo.pro@gmail.com>
2026-09-22 15:21:20 +0200

    FEAT: add 0-minoperation.py , get the minimum operation need to reech the number entered

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#/usr/bin/env python3
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operation += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
